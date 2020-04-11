14f9e1071cea610325e27421f67bd11b9d6b3b87
Steven Curran <steventacurran@gmail.com>
2020-04-11 15:33:34 +0100

    Remove the django nonsense

--- manage.py
+++ manage.py
@@ -3,8 +3,4 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
-
-    from django.core.management import execute_from_command_line
-
-    execute_from_command_line(sys.argv)
+    print("Hello world from the python app of stac")
